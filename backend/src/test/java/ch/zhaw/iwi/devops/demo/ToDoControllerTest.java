784f01ff7786ae126743a9446959fc8404c46d82
Adrian Moser <mosa@zhaw.ch>
2022-12-22 15:21:51 +0100

    update

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -0,0 +1,17 @@
+package ch.zhaw.iwi.devops.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ToDoControllerTest {
+    
+    @Test
+    public void test1() {
+        ToDoController controller = new ToDoController();
+        var todo = new ToDo(1, "t", "d");
+        controller.createTodo(1, todo);
+        assertEquals(1, controller.count());
+    }
+
+}
