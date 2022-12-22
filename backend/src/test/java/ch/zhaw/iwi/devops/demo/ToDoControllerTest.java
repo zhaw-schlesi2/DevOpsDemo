730b4779d0d9b7fda55d5ee881245d1f0aafb601
Adrian Moser <mosa@zhaw.ch>
2022-12-22 15:22:28 +0100

    update

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 public class ToDoControllerTest {
     
     @Test
-    public void test1() {
+    public void testCreate() {
         ToDoController controller = new ToDoController();
         var todo = new ToDo(1, "t", "d");
         controller.createTodo(1, todo);
         assertEquals(1, controller.count());
+        assertEquals(1, controller.todo().size());
     }
 
 }
